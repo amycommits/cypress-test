a5669306736f871c1357d9cb76981a5fcdb2bdcc
duaimei <adetwiler@optoro.com>
2019-10-04 13:38:53 -0400

    starting point for cypress training

---
+++ src/components/KitchenSink.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <button @click="handleClick">CLICK ME</button>
+    <select name="dropdown" id="dropdown">
+      <option value="1">Option 1</option>
+      <option value="2">Option 2</option>
+    </select>
+    <input type="text" v-model="inputVal">
+    <div v-if="inputVal">{{inputVal}}</div>
+    <div v-if="hasBeenClicked">
+      You clicked
+    </div>
+    <div v-else>
+      You have not clicked
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'kitchenSink',
+  data() {
+    return {
+      hasBeenClicked: false,
+      dropdown: null,
+      inputVal: null,
+    };
+  },
+  methods: {
+    handleClick() {
+      this.hasBeenClicked = !this.hasBeenClicked;
+    },
+  },
+};
+</script>
